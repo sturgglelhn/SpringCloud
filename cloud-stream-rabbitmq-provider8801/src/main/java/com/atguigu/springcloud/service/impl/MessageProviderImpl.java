06f7dc824fb6609ebcd223261c9b744628d7901c
Adolf <Adolf@atguigu.com>
2022-09-21 22:24:48 +0800

    SpringCloud3

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/impl/MessageProviderImpl.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+@EnableBinding(Source.class)
+public class MessageProviderImpl implements IMessageProvider {
+
+    @Resource
+    private MessageChannel output;
+
+    @Override
+    public String send() {
+        String serial = UUID.randomUUID().toString();
+        this.output.send(MessageBuilder.withPayload(serial).build());
+        System.out.println("***serial: "+serial);
+
+        return serial;
+    }
+}
